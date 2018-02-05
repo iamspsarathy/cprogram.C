7b82266cec470d2b155a389702b70972c3a5654d
iamspsarathy <35688629+iamspsarathy@users.noreply.github.com>
2018-02-05 16:23:53 +0530

    Create sps6.C

---
+++ sps6.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.g>
+using namespace std;
+int main() {
+    int i, n;
+    cin >> n;
+    for (i=0; i<n; i++) {
+        cout << "hello world\n";
+    }
+    getch()
+    return 0;
+}
